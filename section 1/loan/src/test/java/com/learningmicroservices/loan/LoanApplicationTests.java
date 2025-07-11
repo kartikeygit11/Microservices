96d53a3a62966be2fbc763abd2b1d585cd7bdf3c
kartikeygit11 <kartiksrivastava2307@gmail.com>
2025-07-11 16:19:37 +0530

    loan section added

---
+++ section 1/loan/src/test/java/com/learningmicroservices/loan/LoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningmicroservices.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
