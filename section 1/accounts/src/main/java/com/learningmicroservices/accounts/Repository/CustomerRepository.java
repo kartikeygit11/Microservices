be0995c15ede1f3d7bfca7c0bc09733bdcd56576
kartikeygit11 <kartiksrivastava2307@gmail.com>
2025-07-14 02:41:06 +0530

    repo creation

---
+++ section 1/accounts/src/main/java/com/learningmicroservices/accounts/Repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.learningmicroservices.accounts.Repository;
+
+import com.learningmicroservices.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+
+
+}
